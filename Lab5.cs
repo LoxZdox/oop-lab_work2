5a951c24c40071ef679823c93b0e0b021ab32207
LoxZdox <gidropony1905@gmail.com>
2021-09-13 23:44:59 +0300

    Пятый вариант полностью готов

--- Lab5.cs
+++ Lab5.cs
@@ -22,10 +22,18 @@ namespace lab_work_2
             for(int i = 0; i < 5; i++){
                 array[i]=Convert.ToInt32(Console.ReadLine());
                 temp = array[i];
+                max = array[i];
+                min = array[i];
             }
             Console.WriteLine("Here`s your elements: ");
             for(int i = 0; i < 5; i++){
                 Console.Write($"{array[i]}, ");
+                if(max<=array[i]){
+                    max = array[i];
+                }
+                if(min>=array[i]){
+                    min = array[i];
+                }
             }
             //This is bubble sort, so the second cycle is for the worst scenario. O(n^2)
             for(int j = 0; j < 5; j++){
@@ -41,6 +49,7 @@ namespace lab_work_2
             for(int i = 0; i < 5; i++){
                 Console.Write($"{array[i]}, ");
             }
+            Console.WriteLine($"\nMax integer: {max}, min integer: {min}");
         }
     }
 }
