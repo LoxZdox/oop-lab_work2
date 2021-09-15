5b8487cabafa644ef32f76949c75d860f9f42822
LoxZdox <gidropony1905@gmail.com>
2021-09-15 23:18:19 +0300

    Добавила конструкторы.

--- Lab1.cs
+++ Lab1.cs
@@ -9,9 +9,21 @@ namespace lab_work_2
     class Class1{
         public string name;
         public int age;
+        public readonly string text;
         public Class1(){
-            age = 8;
-            name = "Даня";
+            age = 18;
+            name = "Peter";
+            text = " He lives in a small town.";
+        }
+        public Class1(int newage){
+            age = newage;
+            name = "Peter";
+            text = " He lives in a small town.";
+        }
+        public Class1(int newage, string newname){
+            age = newage;
+            name = newname;
+            text = " He lives in a big town.";
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,8 +11,12 @@ namespace lab_work_2
     {
         public static void Main(string[] args)
         {
-            Class1 obj = new Class1();
-            Console.WriteLine($"Дане {obj.age} лет. {obj.name}- малявка.");
+            Class1 obj1 = new Class1();
+            Console.WriteLine($"He`s {obj1.age} years old, his name is {obj1.name}.{obj1.text}");
+            Class1 obj12 = new Class1(13);
+            Console.WriteLine($"He`s {obj12.age} years old, his name is {obj12.name}.{obj12.text}");
+            Class1 obj13 = new Class1(13, "Damir");
+            Console.WriteLine($"He`s {obj13.age} years old, his name is {obj13.name}.{obj13.text}");
         }
     }
 }
